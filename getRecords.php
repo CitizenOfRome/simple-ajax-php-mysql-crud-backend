3eef3e0cf3380f02997f8d532d025f4bfec441b3
Sathvik P <psathvik@gmail.com>
2017-12-27 13:05:31 +0530

    Added pagination

--- getRecords.php
+++ getRecords.php
@@ -6,6 +6,7 @@ try {
     $searchName = $_GET['searchName'];
     $searchSpecialization = $_GET['searchSpecialization'];
     $searchAge = $_GET['searchAge'];
+    $page = intval($_GET['page']);
     $query = "SELECT * FROM students ";
     if(!empty($searchName) || !empty($searchSpecialization) || !empty($searchAge)) {
         $query .= 'WHERE ';
@@ -21,21 +22,33 @@ try {
         }
         $query .= implode(' AND ', $queryParts);
     }
+    $queryWithoutLimitAndOffset = $query;
+    $query .= ' LIMIT 5 ';
+    $query .= ' OFFSET '. (($page-1)*5);
     $stmt = $conn->prepare($query);
+    $stmtWithoutLimitAndOffset = $conn->prepare($queryWithoutLimitAndOffset);
     if(!empty($searchName)) {
         $stmt->bindParam(':name', $searchName);
+        $stmtWithoutLimitAndOffset->bindParam(':name', $searchName);
     }
     if(!empty($searchSpecialization)) {
         $stmt->bindParam(':specialization', $searchSpecialization);
+        $stmtWithoutLimitAndOffset->bindParam(':specialization', $searchSpecialization);
     }
     if(!empty($searchAge)) {
         $stmt->bindParam(':age', $searchAge);
+        $stmtWithoutLimitAndOffset->bindParam(':age', $searchAge);
     }
     $success = $stmt->execute();
+    $stmtWithoutLimitAndOffset->execute();
+    $count = $stmtWithoutLimitAndOffset->rowCount();
 
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo json_encode($result, JSON_FORCE_OBJECT);
+    echo json_encode(array(
+        'data' => $result,
+        'pagesCount' => ceil($count/5)
+    ), JSON_FORCE_OBJECT);
 } catch(PDOException $e) {
     echo json_encode(array('message' => "Error: " . $e->getMessage(), 'success' => false));
 }
